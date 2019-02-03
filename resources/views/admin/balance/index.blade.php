f5a6a00336b95537f91cd27ac312e6c6b59246af
gabriel <gabrielthe13@gmail.com>
2019-02-03 11:03:02 -0200

    add

---
+++ resources/views/admin/balance/index.blade.php
@@ -0,0 +1,22 @@
+@extends('adminlte::page')
+
+@section('title', 'Saldo')
+
+@section('content_header')
+    <h1>Saldo</h1>
+    <ol class="breadcrumb">
+    	<li><a href="">Dashboard</a></li>
+    	<li><a href="">Saldo</a></li>
+    </ol>
+@stop
+
+@section('content')
+    <div class="box">
+    	<div class="box-header">
+    		Header
+    	</div>
+    	<div class="box-body">
+    		Body
+    	</div>
+    </div>
+@stop
